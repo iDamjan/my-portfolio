abcb675ecfe04750864deeec6f5982986e88dbc6
Damjan Stojanovski <damjan.stojanovski123@gmail.com>
2022-11-29 18:31:03 +0100

    button styles on play snake game page updated

--- src/pages/snakeGame/NavigationButton.jsx
+++ src/pages/snakeGame/NavigationButton.jsx
@@ -1,4 +1,4 @@
-import {useEffect} from "react";
+import { useEffect } from "react";
 import classes from "./SnakeGame.module.scss";
 import Button from "@mui/material/Button";
 import { useNavigate } from "react-router-dom";
@@ -7,12 +7,18 @@ export const NavigationButton = () => {
   const navigate = useNavigate();
 
   const navigateHandler = () => {
-    navigate("/home")
-  }
-  
+    navigate("/home");
+  };
+
   return (
-    <Button onClick = {navigateHandler} className={classes.goToPortfolio} variant="outlined">
-      Go to Portfolio
-    </Button>
+    <div className={classes.navigationButton}>
+      <Button
+        onClick={navigateHandler}
+        
+        variant="outlined"
+      >
+        Go to Portfolio
+      </Button>
+    </div>
   );
 };
